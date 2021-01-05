90615edd1c6e5230f5b7b3e61bea093853ef687d
Claire Vita <clairej.vita@gmail.com>
2021-01-04 21:49:43 -0500

    cloudvision initiation

--- server.js
+++ server.js
@@ -10,12 +10,26 @@ app.use(express.static('/'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 var path = require("path");
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
   });
+
+async function quickstart() {
+  // Imports the Google Cloud client library
+  const vision = require('@google-cloud/vision');
+
+  // Creates a client
+  const client = new vision.ImageAnnotatorClient();
+
+  // Performs label detection on the image file
+  const [result] = await client.labelDetection('./resources/wakeupcat.jpg');
+  const labels = result.labelAnnotations;
+  console.log('Labels:');
+  labels.forEach(label => console.log(label.description));
+}
+quickstart();
 
 //This initiates the server.
 app.listen(PORT, function () {
