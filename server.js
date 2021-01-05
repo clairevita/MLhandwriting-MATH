5fd27168bd0bbef13f473db178e44e4b909cb4a8
Claire Vita <clairej.vita@gmail.com>
2021-01-05 18:12:54 -0500

    reverted to previous credentialization

--- server.js
+++ server.js
@@ -18,20 +18,6 @@ app.get("/", function (req, res) {
 
 function buildCreds() {
   require("dotenv").config();
-  let myproject =
-  {
-    "type": "service_account",
-    "project_id": process.env.MYPROJECT_ID,
-    "private_key_id": process.env.MYPROJECT_PRIVKEYID,
-    "private_key": process.env.MYPROJECT_PRIVKEY,
-    "client_email": process.env.MYPROJECT_EMAIL,
-    "client_id": process.env.MYPROJECT_CLIENTID,
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": process.env.MYPROJECT_CERTURL
-  }
-  quickstart(myproject);
 }
 
 buildCreds();
