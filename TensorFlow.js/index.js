a2fed48be56d6789e09bab317f3fae16a60d39f9
Claire Vita <clairej.vita@gmail.com>
2021-01-24 23:26:58 -0500

    compiling function

--- TensorFlow.js/index.js
+++ TensorFlow.js/index.js
@@ -27,4 +27,52 @@ function createModel() {
         activation: 'relu',
         kernelInitializer: 'VarianceScaling'
     }));
+
+    //Pool size of the sliding window
+    //Stides By how many pixels the pool slides over
+    model.add(tf.layers.maxPool2d({
+        poolSize: [2, 2],
+        strides: [2, 2],
+    }))
+
+    //We repeat the process for quality control with different parameters
+    // We remove input shape because it is inherited from our 2d convolution layer
+    model.add(tf.layers.conv2d({
+        kernelSize: 5,
+        filter: 16,
+        strides: 1,
+        activation: 'relu',
+        kernelInitializer: 'VarianceScaling'
+    }));
+    model.add(tf.layers.maxPool2d({
+        poolSize: [2, 2],
+        strides: [2, 2],
+    }));
+
+    //Flatten all of the output layers for further processing
+    model.add(tf.layers.flatten());
+
+    //Creates a dense fully connected layer for final assessment
+    //Units are the number of characters we're searching for (0-9),
+    //Adaptive shape for final assessment of full output
+    //**Softmax for activation function uses probability for concluding what the most probable digit is being represented
+    model.add(tf.layers.dense({
+        units: 10,
+        kernelInitializer: 'VarianceScaling',
+        activation: 'softmax'
+    }));
+
+    createLogEntry('Layers have been created.');
+
+    createLogEntry('Starting compiling process...');
+
+    //Next we're going to compile our model's losses and metrics. This is an assessment function to ensure total success. 
+    //Optimizer used is a Stochastic  Gradient descent property, which eases out our compiling for QA
+    //Calculates loss between final labels and the predictions
+    model.compile({
+        optimizer: tf.train.sgd(0.15),
+        loss: 'cetgoricalCrossentropy'
+    });
+
+    createLogEntry('Compiling completed.');
 }
